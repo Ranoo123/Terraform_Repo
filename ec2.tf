8919ce68ddfa333236aa79846a047f309de6e465
Ranoo123 <165536053+Ranoo123@users.noreply.github.com>
2024-10-13 22:01:58 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,15 @@
 provider "aws" {
   region     = "us-east-1"
-  version  = "5.28.0"
 }
+terraform {
+  required_providers {
+    mycloud = {
+      source  = "mycorp/mycloud"
+      version = "5.28.0"
+    }
+  }
+}
+
 
 resource "aws_instance" "myec2" {
     ami = "ami-0866a3c8686eaeeba"
