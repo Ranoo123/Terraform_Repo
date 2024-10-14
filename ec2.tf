2a41954b1dfdc91caaa67a60436de3d856794481
Ranoo123 <165536053+Ranoo123@users.noreply.github.com>
2024-10-13 22:07:12 -0700

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,15 +3,12 @@ provider "aws" {
 }
 terraform {
   required_providers {
-    mycloud = {
+    myec2 = {
       source  = "hashicorp/aws"
       version = "5.28.0"
     }
   }
 }
-
-
-
 resource "aws_instance" "myec2" {
     ami = "ami-0866a3c8686eaeeba"
     instance_type = "t2.micro"
